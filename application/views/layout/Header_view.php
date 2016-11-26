ead242f5fcbaa338fc377b8071bbebb61b8f1bbe
kaisaya kombut <kaisaya@gmail.com>
2016-11-26 16:53:44 +0700

    lorem*3 เพิ่ม

--- application/views/layout/Header_view.php
+++ application/views/layout/Header_view.php
@@ -8,16 +8,10 @@
     <title>Bootstrap 101 Template</title>
 
     <!-- Bootstrap -->
-    <link href="bower_components/sb-admin-2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('bower_components/sb-admin-2/css/bootstrap.min.css')?>" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="bower_components/animate.css/animate.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('bower_components/font-awesome/css/font-awesome.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('bower_components/animate.css/animate.min.css')?>">
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-static-top">
