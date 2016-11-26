b3c61c879de1af18acfb1d8015573301347c5f55
kaisaya kombut <kaisaya@gmail.com>
2016-11-26 16:01:03 +0700

    add htacces

---
+++ application/controllers/home.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lab
+ * Date: 26/11/2559
+ * Time: 15:59
+ */
+class  home extends  CI_Controller
+{
+    public  function  index(){
+        $this->load->view('home_view');
+    }
+}
