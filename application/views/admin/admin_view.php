8e7d038aa83b0aa1f87fcc228b413b549e166216
kaisaya kombut <kaisaya@gmail.com>
2016-11-27 14:55:55 +0700

    add admin edite routes

--- application/views/admin/admin_view.php
+++ application/views/admin/admin_view.php
@@ -2,7 +2,7 @@
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Blank</h1>
+            <h1 class="page-header">การจัดการหมวดหมู่</h1>
         </div>
         <!-- /.col-lg-12 -->
     </div>
