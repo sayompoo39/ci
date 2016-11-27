3f8430ed2f7d60acd9fb3b6efff871bb7834e08f
kaisaya kombut <kaisaya@gmail.com>
2016-11-27 13:17:06 +0700

    add admin layout

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -9,7 +9,10 @@
 class Admin extends  CI_Controller
 {
     public function  index(){
+        $this->load->view('admin/layout/admin_header_view');
+        $this->load->view('admin/layout/admin_sidebar_view');
         $this->load->view('admin/admin_view');
+        $this->load->view('admin/layout/admin_footer_view');
     }
 
 }
