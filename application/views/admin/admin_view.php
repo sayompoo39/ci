d2d2ed832be24861c0ec98b34c3bb0023e557140
kaisaya kombut <kaisaya@gmail.com>
2016-11-27 12:44:57 +0700

    Add admin view

--- application/views/admin/admin_view.php
+++ application/views/admin/admin_view.php
@@ -12,16 +12,16 @@
     <title>SB Admin 2 - Bootstrap Admin Theme</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="bower_components/sb-admin-2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('bower_components/sb-admin-2/css/bootstrap.min.css')?>" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="bower_components/sb-admin-2/css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('bower_components/sb-admin-2/css/plugins/metisMenu/metisMenu.min.css')?>" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="bower_components/sb-admin-2/css/sb-admin-2.css" rel="stylesheet">
+    <link href="<?php echo base_url('bower_components/sb-admin-2/css/sb-admin-2.css')?>" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="bower_components/sb-admin-2/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('bower_components/sb-admin-2/font-awesome-4.1.0/css/font-awesome.min.css')?>" rel="stylesheet" type="text/css">
 
 </head>
 
@@ -373,16 +373,16 @@
 <!-- /#wrapper -->
 
 <!-- jQuery Version 1.11.0 -->
-<script src="bower_components/sb-admin-2/js/jquery-1.11.0.js"></script>
+<script src="<?php echo base_url('bower_components/sb-admin-2/js/jquery-1.11.0.js')?>"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="bower_components/sb-admin-2/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url('bower_components/sb-admin-2/js/bootstrap.min.js')?>"></script>
 
 <!-- Metis Menu Plugin JavaScript -->
-<script src="bower_components/sb-admin-2/js/plugins/metisMenu/metisMenu.min.js"></script>
+<script src="<?php echo base_url('bower_components/sb-admin-2/js/plugins/metisMenu/metisMenu.min.js')?>'"></script>
 
 <!-- Custom Theme JavaScript -->
-<script src="bower_components/sb-admin-2/js/sb-admin-2.js"></script>
+<script src="<?php echo base_url('bower_components/sb-admin-2/js/sb-admin-2.js')?>"></script>
 
 </body>
 
