bcb510ad5928efd4099a6e7248765f4f4c083cdb
kaisaya kombut <kaisaya@gmail.com>
2016-11-27 15:17:16 +0700

    category view

--- application/views/admin/category_view.php
+++ application/views/admin/category_view.php
@@ -5,6 +5,27 @@
             <h1 class="page-header">จัดการหมวดหมู่ข่าว</h1>
         </div>
         <!-- /.col-lg-12 -->
+        <table class="table">
+            <tr>
+                <th>#</th>
+                <th>ชื่อหมวดหมู่</th>
+                <th>จัดการ</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>กีฬา</td>
+                <td>
+                    <a href="#"class=""btn btn-xs btn-warnning">
+                    <i class"fa fa-edit"></i>แก้ไข
+                    </a>
+                </td>
+                <td>
+                    <a href="#"class=""btn btn-xs btn-danger">
+                    <i class"fa fa-times"></i>ลบ
+                    </a>
+                </td>
+            </tr>
+        </table>
     </div>
     <!-- /.row -->
 </div>
--- application/views/admin/layout/admin_sidebar_view.php
+++ application/views/admin/layout/admin_sidebar_view.php
@@ -29,75 +29,8 @@
                     <li>
                         <a href="comment.html">จัดการความคิดเห็น</a>
                     </li>
-                </ul>
-                <!-- /.nav-second-level -->
-            </li>
-            <li>
-                <a href="tables.html"><i class="fa fa-table fa-fw"></i>ตาราง</a>
-            </li>
-            <li>
-                <a href="forms.html"><i class="fa fa-edit fa-fw"></i> ฟอรั่ม</a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
                     <li>
-                        <a href="panels-wells.html">Panels and Wells</a>
-                    </li>
-                    <li>
-                        <a href="buttons.html">Buttons</a>
-                    </li>
-                    <li>
-                        <a href="notifications.html">Notifications</a>
-                    </li>
-                    <li>
-                        <a href="typography.html">Typography</a>
-                    </li>
-                    <li>
-                        <a href="grid.html">Grid</a>
-                    </li>
-                </ul>
-                <!-- /.nav-second-level -->
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span
-                        class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="#">Second Level Item</a>
-                    </li>
-                    <li>
-                        <a href="#">Second Level Item</a>
-                    </li>
-                    <li>
-                        <a href="#">Third Level <span class="fa arrow"></span></a>
-                        <ul class="nav nav-third-level">
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-                        </ul>
-                        <!-- /.nav-third-level -->
-                    </li>
-                </ul>
-                <!-- /.nav-second-level -->
-            </li>
-            <li class="active">
-                <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a class="active" href="blank.html">Blank Page</a>
-                    </li>
-                    <li>
-                        <a href="login.html">Login Page</a>
+                        <a href="ีuser.html">จัดการผู้ใช้งาน</a>
                     </li>
                 </ul>
                 <!-- /.nav-second-level -->
